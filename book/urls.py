4793733d66a9ed2a4a5d918d78b49f1469349fc6
longngoryu <longngoryu@gmail.com>
2023-10-26 17:41:04 +0700

    add BookDetailApiView

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookApiView
+from .views import BookApiView, BookDetailApiView
 
 urlpatterns = [
-  path("", BookApiView.as_view())
+  path("", BookApiView.as_view()),
+  path("<str:book_id>/", BookDetailApiView.as_view()),
 ]
--- book/views.py
+++ book/views.py
@@ -29,4 +29,48 @@ class BookApiView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-       
+
+class BookDetailApiView(APIView):
+    def get(self, request, book_id, *args, **kwargs):
+        try:
+            queryset = Book.objects.get(id=book_id)
+            serializer = BookSerializer(queryset, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            return Response(
+                {"res": "Object with book id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+    def put(self, request, book_id, *args, **kwargs):
+        try:
+            queryset = Book.objects.get(id=book_id)
+            data = {
+            "title": request.data.get("title"),
+            "author": request.data.get("author"),
+            "description": request.data.get("description"),
+            "price": request.data.get("price"),
+            "quantity_in_stock": request.data.get("quantity_in_stock"),
+            "publication_date": request.data.get("publication_date"),
+            "image_url": request.data.get("image_url"),
+        }
+            serializer = BookSerializer(instance=queryset, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except BookSerializer.DoesNotExist:
+            return Response(
+                {"res": "Put with product id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+    def delete(self, request, book_id, *args, **kwargs):
+        try:
+            queryset = Book.objects.get(id=book_id)
+            queryset.delete()
+            return Response({"res": "Object deleted!"}, status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            return Response(
+                {"res": "Delete with product id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
